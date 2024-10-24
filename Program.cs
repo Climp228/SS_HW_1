643e1a23c5043f3580264a172c3b501587e2adef
Climp228 <climpser@gmail.com>
2024-10-24 20:15:58 +0300

    1 Task

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace SS_HW_1
+{
+    internal class CircleCalculations
+    {
+        public static void Main(string[] args)
+        {
+            int a;
+
+            if (int.TryParse(Console.ReadLine(), out a))
+            {
+                int area = a * a;
+                int perimeter = 4 * a;
+
+                Console.WriteLine("Area: {0}", area);
+                Console.WriteLine("Perimeter: {0}", perimeter);
+            }
+            else
+            {
+                Console.WriteLine("Please, enter an integer value");
+            }
+        }
+    }
+}
